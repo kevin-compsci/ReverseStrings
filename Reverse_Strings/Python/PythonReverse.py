332244707014925f41322d0a56aa728dd8d48a40
Kevin Nguyen <25444103+kevin-compsci@users.noreply.github.com>
2018-04-06 09:24:50 -0500

    Add files via upload

--- Reverse_Strings/Python/PythonReverse.py
+++ Reverse_Strings/Python/PythonReverse.py
@@ -5,13 +5,10 @@
 #
 #How to run (linux):
 #	1.) open command line and cd into directory folder of where this code is
-#	2.) type in "python Python-Shell.py"
+#	2.) type in "python PythonReverse.py"
 
 #import statements
 import sys;
-import array;
-
-#TODO: perform reverse strings!
 
 #function implementation/definition below
 #Reverse function will take an input string and reverse the characters for output
@@ -23,10 +20,12 @@ def Reverse(myString):
 	while(length > -1):
 		result = result + myString[length];
 		length=length-1;
-	print result;
+	return result;
 
 
 #main driver below
 myString = "Hello World";
 myReverseString = Reverse(myString);
+print "Initial: " + myString;
+print "Reverse: " + myReverseString;
 
